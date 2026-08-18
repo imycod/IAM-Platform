8f762b91d358954c864cc205890f29375f87d7d5
wuxs <imycode@qq.com>
2026-08-18 16:14:08 +0800

    redirect route

--- apps/iam-admin/src/router/modules/home.ts
+++ apps/iam-admin/src/router/modules/home.ts
@@ -5,11 +5,12 @@ export default {
   path: "/",
   name: "Home",
   component: Layout,
-  redirect: "/welcome",
+  redirect: "/access/role/index",
   meta: {
     icon: "ep/home-filled",
     title: "首页",
-    rank: 0
+    rank: 0,
+    showLink: false
   },
   children: [
     {
@@ -18,7 +19,7 @@ export default {
       component: () => import("@/views/welcome/index.vue"),
       meta: {
         title: "首页",
-        showLink: VITE_HIDE_HOME === "true" ? false : true
+        showLink: false
       }
     }
   ]
